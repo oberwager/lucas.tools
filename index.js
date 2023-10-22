926a7c9680fb515e782101095aef493354c94fce
Watt3r <git@lucas.tools>
2023-10-22 12:29:20 -0400

    refactor: better timeSince function

--- index.js
+++ index.js
@@ -8,30 +8,23 @@ function titleCase(s) {
   });
 }
 function timeSince(date) {
-  const seconds = Math.floor((new Date() - date) / 1000);
+    const seconds = Math.floor((new Date() - date) / 1000);
 
-  let interval = seconds / 31536000;
+    const timeUnits = [
+        { name: 'year', seconds: 31536000 },
+        { name: 'month', seconds: 2592000 },
+        { name: 'day', seconds: 86400 },
+        { name: 'hour', seconds: 3600 },
+        { name: 'minute', seconds: 60 },
+        { name: 'second', seconds: 1 }
+    ];
 
-  if (interval > 1) {
-    return Math.floor(interval) + " years";
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " months";
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " days";
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " hours";
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
+    for (const unit of timeUnits) {
+        const interval = Math.floor(seconds / unit.seconds);
+        if (interval >= 1) {
+            return `${interval} ${unit.name}${interval > 1 ? 's' : ''}`;
+        }
+    }
 }
 scrollEvent = () => {
   // Check if Lucas Oberwager should be stuck to top
